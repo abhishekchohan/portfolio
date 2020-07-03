7a6ca71e0851fbf10b97022fcbeaed1eea56cbb0
Abhishek Chohan <abhishekchohan1@gmail.com>
2020-07-03 15:41:44 -0400

    nitial commit

---
+++ js/smoothScroll.js
@@ -0,0 +1,8 @@
+// SMOOTH SCROLLING SCRIPT
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000, 'linear');
+});
